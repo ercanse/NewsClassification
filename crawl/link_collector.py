d05c74e1474ecf31189c355a80592249cedfd6b1
Selman Ercan <ercan.s.selman@gmail.com>
2016-04-06 23:10:03 +0200

    get_links_for_month now returns list of article URLs.

--- crawl/link_collector.py
+++ crawl/link_collector.py
@@ -1,21 +1,31 @@
 import lxml.html as html_parser
 import urllib2
 
+
 def get_links_for_year(year):
     """
     """
+    links = []
     for month in range(1, 13):
-        get_links_for_month(year, month)
+        links.extend(get_links_for_month(year, month))
+    return links
 
 
 def get_links_for_month(year, month):
     """
     """
+    links = []
     url = 'http://www.cracked.com/funny-articles.html?date_year=%d&date_month=%d' % (year, month)
     document = html_parser.parse(urllib2.urlopen(url))
-    articles_container = document.xpath('//div[@id="safePlace"]')[0]
-    for element in articles_container.getchildren():
-        print(element.get('class'))
+
+    urls = document.xpath('//div[@id="safePlace"]//a')
+    for url in urls:
+        values = url.values()
+        if 'linkImage' in values:
+            links.append(url.values()[1])
+
+    print(links)
+    return links
 
 if __name__ == '__main__':
     get_links_for_month(2015, 1)
