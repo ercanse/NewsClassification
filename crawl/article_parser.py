063792206cb2c69b3d8677beba9863e0e746d51a
Selman Ercan <ercan.s.selman@gmail.com>
2016-04-07 22:22:56 +0200

    Implemented retrieving and processing of articles; added support for articles spread over multiple pages.

--- crawl/article_parser.py
+++ crawl/article_parser.py
@@ -30,22 +30,34 @@ def process_urls(urls):
     :return: list of article retrieved from URLs
     """
     for url in urls:
-        retrieve_article(url)
+        retrieve_main_article_page(url['url'])
 
 
-def retrieve_article(url):
+def retrieve_main_article_page(url):
     """
     :param url: URL of article to retrieve
     :return: retrieved article
     """
-    print(url['url'])
-    article = html_parser.parse(urllib2.urlopen(url['url']))
-    process_article(article)
+    print(url)
+    article = html_parser.parse(urllib2.urlopen(url))
+    process_article(url, article)
 
 
-def process_article(article):
+def retrieve_subsequent_article_page(url):
+    """
+    :param url: URL of article to retrieve
+    :return: retrieved article
+    """
+    print(url)
+    article = html_parser.parse(urllib2.urlopen(url))
+    headings, text = process_article_text(article)
+    return headings, text
+
+
+def process_article(url, article):
     """
     Parses article and stores its details in the database.
+    :param url: article URL
     :param article: article to process
     """
     title = article.xpath('//section[@class="body"]//h1')[0].text
@@ -56,9 +68,33 @@ def process_article(article):
 
     num_comments = article.xpath('//section[@class="header subheader"]//span[@id="commentCounts"]')[0].text
 
-    print title, author, num_views, num_comments, '\n'
+    headings, text = process_article_text(article)
 
+    pagination_elements = article.xpath('//span[@class="paginationNumber"]')
+    if pagination_elements:
+        pages = int(pagination_elements[1].text)
+        for page_number in range(2, pages + 1):
+            next_page_url = url.replace('.html', '') + '_p%d.html' % page_number
+            headings_next, text_next = retrieve_subsequent_article_page(next_page_url)
+            headings.extend(headings_next)
+            text.extend(text_next)
+
+    return author, title, headings, text, num_views, num_comments
+
+
+def process_article_text(article):
+    headings = []
+    heading_elements = article.xpath('//section[@class="body"]//section//h2')
+    for sub_heading in heading_elements:
+        headings.append(sub_heading.text)
+
+    text = []
+    paragraphs = article.xpath('//section[@class="body"]//section//p')
+    for text_element in paragraphs:
+        text.append(text_element.text)
+
+    return headings, text
 
 if __name__ == '__main__':
     urls = load_urls()
-    process_urls(urls[:5])
+    process_urls(urls[500:501])
