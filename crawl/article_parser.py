0c60d457fb3b9431c7a40ab88fdaab6e55a3e10f
Selman Ercan <ercan.s.selman@gmail.com>
2016-04-12 22:21:24 +0200

    Improved handling of URLs that couldn't be retrieved or processed.

--- crawl/article_parser.py
+++ crawl/article_parser.py
@@ -4,9 +4,11 @@ Script to load article URLs from the database, retrieve the articles, and extrac
 """
 
 import lxml.html as html_parser
-import pymongo
 import urllib2
 
+from optparse import OptionParser
+
+from bson.dbref import DBRef
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
@@ -16,37 +18,58 @@ urls_collection_name = 'urls'
 failed_urls_collection_name = 'urls_failed'
 articles_collection_name = 'articles_raw'
 
+db = Database(MongoClient(), db_name)
+urls_collection = Collection(db, urls_collection_name)
+articles_collection = Collection(db, articles_collection_name)
+failed_urls_collection = Collection(db, failed_urls_collection_name)
 
-def load_urls():
+
+def load_urls(only_failed=False):
 	"""
 	:return: all URL documents in collection 'db_name.collection_name'.
 	"""
-	db = Database(MongoClient(), db_name)
-	collection = Collection(db, urls_collection_name)
-	return collection.find().sort([('year', pymongo.ASCENDING), ('month', pymongo.ASCENDING)])
+	urls = []
+
+	if not only_failed:
+		for url_to_process in urls_collection.find():
+			urls.append(url_to_process)
+	for url_failed in failed_urls_collection.find():
+		url_failed['failed'] = True
+		urls.append(url_failed)
+	return urls
 
 
 def process_urls(urls):
 	"""
 	Retrieves articles indicated by the URLs in urls and extracts information from them.
 	:param urls: article URLs to process
-	:return: list of article retrieved from URLs and list of URLs that couldn't be processed correctly
+	:return: list of articles retrieved from URLs and list of URLs that couldn't be processed correctly
 	"""
 	failed_urls = []
 	articles = []
-	num_articles = urls.count()
+	num_articles = len(urls)
 	count = 1
 
 	for url in urls:
 		try:
 			print 'Retrieving article %d of %d at URL \'%s\'...' % (count, num_articles, url['url'])
-			article_details = retrieve_article_page(url['url'])
-		except IndexError:
-			print 'Couldn\'t retrieve article at URL \'%s\'...' % url['url']
-			failed_urls.append(url)
-		articles.append(article_details)
-		for key, value in article_details.iteritems():
-			print key, value
+			article_details = retrieve_article_page(url)
+		except urllib2.HTTPError:
+			print 'Couldn\'t retrieve article at URL \'%s\'.' % url['url']
+			if 'failed' not in url:
+				failed_urls.append(url)
+				failed_urls_collection.insert_one(url)
+		except (IndexError, UnicodeDecodeError):
+			print 'Couldn\'t process article at URL \'%s\'.' % url['url']
+			if 'failed' not in url:
+				failed_urls.append(url)
+				failed_urls_collection.insert_one(url)
+		else:
+			articles.append(article_details)
+			articles_collection.insert_one(article_details)
+			# Remove entry from collection of failed urls
+			if 'failed' in url:
+				failed_urls_collection.delete_one({'_id': url['_id']})
 		count += 1
 	return articles, failed_urls
 
@@ -57,7 +80,7 @@ def retrieve_article_page(url):
 	:param url: URL of article to retrieve
 	:return: retrieved article
 	"""
-	article = html_parser.parse(urllib2.urlopen(url))
+	article = html_parser.parse(urllib2.urlopen(url['url']))
 	article_details = process_article(url, article)
 	return article_details
 
@@ -93,12 +116,13 @@ def process_article(url, article):
 	if pagination_elements:
 		pages = int(pagination_elements[1].text)
 		for page_number in range(2, pages + 1):
-			next_page_url = url.replace('.html', '') + '_p%d.html' % page_number
+			next_page_url = url['url'].replace('.html', '') + '_p%d.html' % page_number
 			headings_next, text_next = retrieve_subsequent_article_page(next_page_url)
 			headings.extend(headings_next)
 			text.extend(text_next)
 
 	return {
+		'url': DBRef(urls_collection_name, url['_id']),
 		'author': author,
 		'title': title,
 		'headings': headings,
@@ -126,25 +150,14 @@ def process_article_text(article):
 	return headings, text
 
 
-def save_articles_and_failed_urls(articles, failed_urls):
-	"""
-	Inserts articles into the database.
-	:param articles: list of dicts containing article details
-	:param failed_urls: list of URLs that couldn't be processed correctly
-	"""
-	collection = Collection(db, articles_collection_name)
-	collection.insert_many(articles)
-	print "Inserted %d articles into '%s.%s'." % (len(articles), db_name, articles_collection_name)
-
-	if failed_urls:
-		collection = Collection(db, failed_urls_collection_name)
-		collection.insert_many(failed_urls)
-		print "Inserted %d failed URLs into '%s.%s'." % (len(failed_urls), db_name, failed_urls_collection_name)
-
-
 if __name__ == '__main__':
+	parser = OptionParser()
+	parser.add_option(
+		"--only_failed", action="store_true", dest="only_failed", default=False, help="Process only failed urls."
+	)
+	(options, args) = parser.parse_args()
+
 	db = Database(MongoClient(), db_name)
 
-	urls_list = load_urls()
-	articles_list, failed_urls_list = process_urls(urls_list[0:100])
-	save_articles_and_failed_urls(articles_list, failed_urls_list)
+	urls_list = load_urls(only_failed=options.only_failed)
+	articles_list, failed_urls_list = process_urls(urls_list)
