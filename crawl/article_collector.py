fbf36d49d3f28a133db18c8cdaed57dc89ff5b09
Selman Ercan <ercan.s.selman@gmail.com>
2016-05-27 21:56:18 +0200

    Implemented filtering of found article URLs.

--- crawl/article_collector.py
+++ crawl/article_collector.py
@@ -4,6 +4,7 @@ Script to retrieve news articles from NU.nl and insert them into a MongoDB datab
 
 import lxml.html as html_parser
 import urllib2
+import re
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
@@ -12,27 +13,27 @@ from pymongo.database import Database
 db_name = 'nu'
 collection_name = 'articles'
 
-
 def get_articles():
     """
     Retrieves all articles not yet in the database and inserts them into the database.
     """
     print "Retrieving articles..."
-    urls = []
+    articles = []
     url = 'http://www.nu.nl/'
     document = html_parser.parse(urllib2.urlopen(url))
 
-    # Article URLs are contained in <a> elements inside <div id="safePlace">
-    url_elements = document.xpath('//div[@id="safePlace"]//a')
+    # Article URLs are contained in <a> elements inside <div class="column-content">
+    url_elements = document.xpath('//div[@class="column-content"]//a')
     for url in url_elements:
         values = url.values()
-        if 'linkImage' in values:
-            urls.append({
-                'url': url.attrib['href']
-            })
-    print "Retrieved %d articles." % (len(urls))
+        if len(values) == 2:
+            url = values[0]
+            if not 'advertorial' in url and re.match('/.+/\d+/.+', url):
+                print url
+                articles.append(url)
+    print "Retrieved %d articles." % (len(articles))
 
-    return urls
+    return articles
 
 
 def get_number_of_comments():
@@ -57,4 +58,4 @@ def save_articles(urls):
 if __name__ == '__main__':
     # Retrieve articles and insert them into the database
     articles_dict = get_articles()
-    save_articles(articles_dict)
+    #save_articles(articles_dict)
