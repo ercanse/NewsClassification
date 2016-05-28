72ba9e0018a625131f0c50ccdbe336afe024abe6
Selman Ercan <ercan.s.selman@gmail.com>
2016-05-28 23:06:15 +0200

    Created script for article preprocessing.

---
+++ preprocessing/process_articles.py
@@ -0,0 +1,14 @@
+"""
+Script to preprocess NU.nl news articles.
+
+Resources:
+http://stackoverflow.com/questions/5486337/how-to-remove-stop-words-using-nltk-or-python?rq=1.
+"""
+
+
+def preprocess():
+    pass
+
+
+if __name__ == '__main__':
+    preprocess()
