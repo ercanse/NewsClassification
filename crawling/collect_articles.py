5329dc9e604a8a33e3570d5095c1eabf7fe9128c
Selman Ercan <ercan.s.selman@gmail.com>
2016-05-29 14:35:40 +0200

    Refactoring.

--- crawling/collect_articles.py
+++ crawling/collect_articles.py
@@ -20,23 +20,43 @@ collection = Collection(db, collection_name)
 base_url = 'http://www.nu.nl'
 
 
-def get_articles():
+def collect_articles():
     """
-    Retrieves all articles not yet in the database and inserts them into the database.
+    :return:
     """
-    print "Retrieving articles..."
-    # Determine URLs already processed
+    retrieved_urls = get_retrieved_urls()
+    front_page = get_front_page()
+    articles = get_articles(front_page, retrieved_urls)
+    save_articles(articles)
+
+
+def get_retrieved_urls():
+    """
+    :return: list of URLs of all articles in the database
+    """
     retrieved_articles = collection.find({}, {'news_url': 1})
     retrieved_urls = [article['news_url'] for article in retrieved_articles]
+    print 'Found %d URLs already retrieved...\n' % len(retrieved_urls)
+    return retrieved_urls
 
+
+def get_front_page():
+    """
+    :return: page at URL 'base_url'
+    """
+    print "Checking for articles on %s..." % base_url
+    return download_page(base_url)
+
+
+def get_articles(page, retrieved_urls):
+    """
+    :return: Retrieves all articles on 'page', the URLs of which are not in 'retrieved_urls'.
+    """
     num_links_processed = 0
     articles = []
-    front_page = download_page(base_url)
-    if front_page is None:
-        exit()
-
     # Article URLs are contained in <a> elements inside <div class="column-content">
-    url_elements = front_page.xpath('//div[@class="column-content"]//a')
+    url_elements = page.xpath('//div[@class="column-content"]//a')
+
     for url in url_elements:
         values = url.values()
         if len(values) == 2:
@@ -47,16 +67,16 @@ def get_articles():
                 article_url = '%s%s' % (base_url, url)
                 # Skip if already processed
                 if article_url not in retrieved_urls:
-                    article = get_article_contents('%s%s' % (base_url, url))
+                    article = process_article('%s%s' % (base_url, url))
                     if article is not None:
                         articles.append(article)
-    print "Retrieved %d new articles, skipped %d existing ones." \
-          % (len(articles), num_links_processed - len(articles))
 
+    print "Retrieved %d new articles, skipped %d existing ones.\n" \
+          % (len(articles), num_links_processed - len(articles))
     return articles
 
 
-def get_article_contents(url):
+def process_article(url):
     """
     :param url:
     :return:
@@ -68,6 +88,17 @@ def get_article_contents(url):
     if article is None:
         return article
 
+    article_contents = extract_article_contents(article)
+    if article_contents is not None:
+        article_contents['news_url'] = url
+    return article_contents
+
+
+def extract_article_contents(article):
+    """
+    :param article:
+    :return:
+    """
     # Extract publication date
     published = article.xpath('//span[@class="published"]//span[@class="small"]')[0].text.strip()
     published_date = datetime.strptime(published, '%d-%m-%y %H:%M')
@@ -87,11 +118,11 @@ def get_article_contents(url):
     )[0].attrib['href']
     # Skip article if the comments URL couldn't be extracted properly
     if comments_url.startswith('http://www.nujij.nl/jij.lynkx/?u=http') and 'slideshow' in comments_url:
+        print 'Couldn\'t find comments URL for article.'
         return None
 
     return dict(
         published=published_date,
-        news_url=url,
         comments_url=comments_url,
         title=title,
         text=text,
@@ -106,14 +137,15 @@ def get_number_of_comments():
     """
     date = datetime.now() - timedelta(days=1)
     articles = collection.find({'published': {'$lt': date}, 'num_comments': None})
-    print '\nUpdating number of comments for %d articles...' % articles.count()
+    print 'Updating number of comments for %d articles...' % articles.count()
 
     num_comments_updated = 0
 
     for article in articles:
         comments_url = article['comments_url']
-        # Delete article if the comments URL
+        # Delete article if the comments URL is invalid
         if comments_url.startswith('http://www.nujij.nl/jij.lynkx/?u=http') and 'slideshow' in comments_url:
+            print 'Deleting article with invalid comments URL...'
             collection.delete_one({'_id': article['_id']})
         else:
             # Retrieve comments page
@@ -121,15 +153,15 @@ def get_number_of_comments():
             comments_page = download_page(comments_url)
             if comments_page is None:
                 continue
-
+            # Search for element containing number of comments
             comments_element = comments_page.find('//span[@class="bericht-reacties"]')
             if comments_element is None:
                 print 'Couldn\'t find comments, deleting article...'
                 collection.delete_one({'_id': article['_id']})
                 continue
 
-            comments_text = comments_element.text.strip()
             # Update article with the number of comments it has received
+            comments_text = comments_element.text.strip()
             num_comments = int(comments_text.split(' ')[0])
             collection.update_one({'_id': article['_id']}, {'$set': {'num_comments': num_comments}})
             num_comments_updated += 1
@@ -147,7 +179,7 @@ def download_page(url):
     try:
         page = html_parser.parse(urllib2.urlopen(url))
     except urllib2.URLError:
-        print 'Couldn\'t access %s.' % url
+        print 'Could not access page at URL %s.' % url
     return page
 
 
@@ -165,8 +197,7 @@ def run():
     """
     Retrieves articles and saves them to the database.
     """
-    articles = get_articles()
-    save_articles(articles)
+    collect_articles()
     get_number_of_comments()
 
 
