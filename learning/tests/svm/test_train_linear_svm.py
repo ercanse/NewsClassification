a652bbdc011a4e5e060397e8c4f9450f284aaa92
Selman Ercan <ercan.s.selman@gmail.com>
2016-06-11 15:06:09 +0200

    Added script for training a linear SVM.

--- learning/tests/svm/test_train_linear_svm.py
+++ learning/tests/svm/test_train_linear_svm.py
@@ -2,18 +2,13 @@ import numpy
 
 from unittest import TestCase
 
-from learning.naive_bayes.train_multinomial_nb import evaluate_classifier_using_fixed_split
-from learning.naive_bayes.train_multinomial_nb import evaluate_classifier_using_repeated_cross_validation
+from learning.svm.train_linear_svm import evaluate_classifier_using_repeated_cross_validation
 
 
-class TestTrainMultinomialNB(TestCase):
+class TestTrainLinearMultinomialNB(TestCase):
     def setUp(self):
         self.feature_vectors = numpy.array([[0, 0], [0, 1], [1, 0], [1, 1]])
         self.target_values = numpy.array(['very_low', 'very_low', 'medium', 'medium'])
-
-    def test_evaluate_classifier_using_fixed_split_returns_float(self):
-        score = evaluate_classifier_using_fixed_split(self.feature_vectors, self.target_values)
-        self.assertIsInstance(score, float)
 
     def test_evaluate_classifier_using_repeated_cross_validation_returns_float(self):
         score = evaluate_classifier_using_repeated_cross_validation(self.feature_vectors, self.target_values, n_folds=2,
---
+++ learning/tests/test_prepare_data.py
@@ -0,0 +1,28 @@
+import numpy
+
+from unittest import TestCase
+
+from learning.naive_bayes.train_multinomial_nb import get_feature_vectors_and_target_values
+
+
+class TestPrepareData(TestCase):
+    def setUp(self):
+        self.feature_vectors_dicts = [{'feature_vector': [0, 1], 'num_comments': 10},
+                                      {'feature_vector': [1, 0], 'num_comments': 20}]
+        self.target_classes = {'very_low': {'start': 0, 'end': 10}, 'low': {'start': 11, 'end': 20}}
+
+    def test_get_feature_vectors_and_target_values_returns_ndarray_of_vectors(self):
+        vectors, _ = get_feature_vectors_and_target_values(self.feature_vectors_dicts, self.target_classes)
+        self.assertIsInstance(vectors, numpy.ndarray)
+
+    def test_get_feature_vectors_and_target_values_returns_ndarray_of_values(self):
+        _, values = get_feature_vectors_and_target_values(self.feature_vectors_dicts, self.target_classes)
+        self.assertIsInstance(values, numpy.ndarray)
+
+    def test_get_feature_vectors_and_target_values_returns_values_array_of_correct_shape(self):
+        _, values = get_feature_vectors_and_target_values(self.feature_vectors_dicts, self.target_classes)
+        self.assertTupleEqual((len(self.target_classes),), values.shape)
+
+    def test_get_feature_vectors_and_target_values_returns_correct_values(self):
+        _, values = get_feature_vectors_and_target_values(self.feature_vectors_dicts, self.target_classes)
+        self.assertListEqual(['very_low', 'low'], values.tolist())
